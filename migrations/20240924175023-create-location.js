6fbb77f0f2471e264df9e4577e9a906d7441b7f0
Paulo-NB <nettobrito55@gmailcom>
2024-09-24 15:20:45 -0300

    meu

--- migrations/20240924175023-create-location.js
+++ migrations/20240924175023-create-location.js
@@ -1,22 +1,56 @@
 'use strict';
 
+const { type } = require('os');
+const sequelize = require('../config/database');
+const { create } = require('../models/location');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('Locations',{
+      id:{
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey:true,
+        type: Sequelize.INTEGER
+      },
+      iduser:{
+        allowNull: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER
+      },
+      idcourt:{
+        allowNull: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER
+      },
+      idpayment:{
+        allowNull: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER
+      },
+      date:{
+        allowNull: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER
+      },
+      createAt:{
+        allowNull: false,
+        type: Sequelize.DATE
+        
+      },
+      updateAt:{
+        allowNull: false,
+        type: Sequelize.DATE
+
+      }
+    })
+
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    
+     await queryInterface.dropTable('Locations');
+     
   }
 };
