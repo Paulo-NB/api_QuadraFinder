2c215e2b9144eaf35fcbf1e9ce92745d5995388d
juanvictor-jv <juansilva282129@gmail.com>
2024-10-03 14:45:36 -0300

    subindo quadra

--- controllers/quadra.js
+++ controllers/quadra.js
@@ -1,20 +1,38 @@
 
 const Quadra = require('../models/quadra')
 
-async function create_quadra(publicplace, zipcode, photos, type, name, state, city, neighborhood){
+async function create_quadra(req, res){
+    const { publicplace, zipcode, photos, type, name, state, city, neighborhood} = req.body
+
+    if(!publicplace || !zipcode || !photos || !type || !name || !state || !city || ! neighborhood){
+        return res.status(400).json({
+            massege: 'Todos os campos são obrigatórios'
+        })
+    }
     
     const quadra = await Quadra.create({publicplace, zipcode, photos, type, name, state, city, neighborhood})
 
-    return quadra
+    return res.status(201).json({
+        message:"criado",
+        db: quadra
+    })
 
 }
 
-async function update_quadra(id, publicplace, zipcode, photos, type, name, state, city, neighborhood){
-   const quadra = await Quadra.findByPk(id)
+async function update_quadra(req, res){
+   
+    const id = parseInt(req.params.id)
+
+    const {publicplace, zipcode, photos, type, name, state, city, neighborhood} = req.body
+
+
+
+    const quadra = await Quadra.findByPk(id)
 
     if(!quadra){
-        return {status: 404, msg: "Não encontrado"}
+        return res.status(404).json("Não encontrado")
     }
+
 
     if(publicplace) quadra.publicplace = publicplace
     if(zipcode) quadra.zipcode = zipcode
@@ -27,24 +45,30 @@ async function update_quadra(id, publicplace, zipcode, photos, type, name, state
 
     await quadra.save()
 
-    return {status: 200, msg: quadra}
+    return res.status(203).json({
+        message:"atualizado",
+        db: quadra
+    })
 }
 
 
-async function delete_quadra(id){
+async function delete_quadra(req, res){
+    const id = parseInt(req.params.id)
     const quadra = await Quadra.findByPk(id)
 
     if(!quadra){
-        return false
+        return res.status(404).json("Não encontrado")
     }
 
     await quadra.destroy()
 
-    return true
+    return res.status(201).json("Foi de base")
 }
 
-async function read_quadra(){
-    return await Quadra.findAll
+async function read_quadra(req, res){
+    return res.status(200).json({
+        massege: 'Sucesso', list_quadra:await quadra.findAll()
+    })
 }
 
 
--- routes/quadra.js
+++ routes/quadra.js
@@ -1,57 +1,26 @@
 const express = require('express')
 const router = express.Router()
 const cquadra = require('../controllers/quadra')
+const cquadra = require('../controllers/quadra')
 
 
 
 
-router.post("/create", (req, res) => {
-    const { publicplace, zipcode, photos, type, name, state, city, neighborhood} = req.body
-
-    if(!publicplace || !zipcode || !photos || !type || !name || !state || !city || ! neighborhood){
-        return res.status(400).json({
-            massege: 'Todos os campos são obrigatórios'
-        })
-    }
-
-    const quadra = cquadra.create_quadra( publicplace, zipcode, photos, type, name, state, city, neighborhood)
-    return res.status(200).json({
-        massege: 'Sucesso', quadra_created: quadra
-    })
-
-} )
+router.post("/create", cquadra.create_quadra)
 
 
 
-
-router.get("/read", (req, res) =>{
-    return res.status(200).json({
-        massege: 'Sucesso', list_quadra: cquadra.read_quadra()
-    })
-})
+router.get("/read", cquadra.read_quadra)
+   
 
 
 
-router.put("/update/:id", (req, res) => {
-    const id = parseInt(req.params.id)
-
-    const {publicplace, zipcode, photos, type, name, state, city, neighborhood} = req.body
-
-    let retorno = cquadra.update_quadra(id, publicplace, zipcode, photos, type, name, state, city, neighborhood)
-    return res.status(retorno.status).json(retorno.msg)
-
-})
+router.put("/update/:id",cquadra.update_quadra)
+   
 
 
 
-router.delete("/del/:id", (req, res) =>{
-    const id = parseInt(req.params.id)
-    if(cquadra.delete_quadra(id)){
-        return res.status(201).json("Foi de base")
-    }else{
-        return res.status(404).json("Não encontrado")
-    }
-} )
+router.delete("/del/:id",cquadra.delete_quadra)
 
 
 module.exports = router
