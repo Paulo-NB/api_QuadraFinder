d82cb0f26dc45bc3cbb82270762f45d07799a10c
thayananJacob <thayanan06jacob@gmail.com>
2024-10-03 14:31:23 -0300

    subi payment

--- controllers/payment.js
+++ controllers/payment.js
@@ -4,33 +4,51 @@ const Payment = require('../models/payment')
 
 
 
- async function create_payment(method, total, date, iduser, idlocation){
-  
+ async function create_payment(req, res){
+    const {method, total, date, iduser, idlocation} = req.body
+
+    if(!method ||  !total ||  !date || !iduser || !idlocation){
+        return res.status(400).json({ 
+            message: 'Todos os campos são obrigatorios'
+        })
+    }
+
 
     const payment = await Payment.create ({method, total, date, iduser, idlocation})
      
+<<<<<<< HEAD
         
 
     return payment 
+=======
+    return res.status(200).json({
+        message:"sucesso",
+        db: payment
+    }) 
+>>>>>>> afc3a7a (subi payment)
 }
 
 
-async function delete_payment(id){
+async function delete_payment(req, res){
+    const id = parseInt(req.params.id)
     const payment = await Payment.findByPK(id)
 
     if(!payment){
-        return false
+        return res.status(404).json("Não encontrado")
     }
-      
-    await Pyment.destroy()
+    
+    await Payment.destroy()
 
-    return true
-
+    return res.status(201).json("Foi de baserado")
 }
 
 
-async function read_payment(){
-    return await Payment.findAll
+async function read_payment(req, res){
+    return res.status(200).json({
+        message: 'Sucesso', list_payment: await Payment.findAll()
+     
+
+    })
 }
 
 module.exports = {
--- routes/payment.js
+++ routes/payment.js
@@ -6,36 +6,10 @@ const cpayment = require('../controllers/payment')
 
 
 
-router.post("/create", (req, res) => {
-    const {method, total, date, iduser, idlocation} = req.body
+router.post("/create", cpayment.create_payment)
 
-    if(!method ||  !total ||  !date || !iduser || !idlocation){
-        return res.status(400).json({ 
-            message: 'Todos os campos são obrigatorios'
-        })
-    }
+router.get("/read",cpayment.read_payment)
 
-    const payment = cpayment.create_payment(method, total, date, iduser, idlocation)
-
-    return res.status(200).json({ 
-        message: 'Sucesso', payment_created: payment
-    })
-})
-
-router.get("/read",(req, res) =>{
-    return res.status(200).json({
-        message: 'Sucesso', list_payment: cpayment.read_payment()
-    })
-})
-
-
-router.delete("/del/:id", (req, res)=>{
-    const id = parseInt(req.params.id)
-    if(cpayment.delete_payment(id)){
-        return res.status(201).json("Foi de base")
-    }else{
-        return res.status(404).json("Não encontrado")
-    }
-})
+router.delete("/del/:id", cpayment.delete_payment)
 
 module.exports = router 
