1c3976fc0fd42542fc384a1697a1c96891c18872
juanvictor-jv <juansilva282129@gmail.com>
2025-02-11 16:42:24 -0300

    teste vitor funfando

--- test.js
+++ test.js
@@ -62,15 +62,84 @@ describe('Testando a API', () => {
         expect(response.status).toBe(200);
         expect(response.body).toHaveProperty('db');
     });
-
-    it('Deve deletar um usúario', async () => {
+    let quadraId;
+  
+    it('Deve criar uma quadra', async () => {
         const res = await request(app)
-            .delete(`/payment/del/${paymentid}`)
+            .post('/quadra/create')
+            .send({
+                publicplace: 'Rua Exemplo',
+                zipcode: '12345-678',
+                photos: 'link-da-foto',
+                type: 'society',
+                name: 'Quadra Exemplo',
+                state: 'SP',
+                city: 'São Paulo',
+                neighborhood: 'Centro',
+                preco: 'R$100,00',
+                alugado: 'não',
+                naoalugado: 'sim'
+            });
 
         expect(res.status).toBe(201);
-    })
+        expect(res.body).toHaveProperty("message", "criado");
+        expect(res.body).toHaveProperty('db');
+        quadraId = res.body.db.id;
+    });
 
+    it('Deve buscar todas as quadras', async () => {
+        const res = await request(app)
+            .get('/quadra/read');
+            
 
+        expect(res.status).toBe(200);
+        expect(Array.isArray(res.body)).toBe(false);
+    });
 
+    it('Deve buscar uma quadra pelo nome', async () => {
+        const res = await request(app)
+            .get(`/quadra/read`)
+            
+
+        expect(res.status).toBe(200);
+        expect(res.body).toHaveProperty('db');
+    });
+
+    it('Deve buscar uma quadra pelo ID', async () => {
+        const res = await request(app)
+            .get(`/quadra/show/${quadraId}`)
+            
+
+        expect(res.status).toBe(202);
+        expect(res.body).toHaveProperty('db.id');
+    });
+
+    it('Deve atualizar uma quadra', async () => {
+        const res = await request(app)
+            .put(`/quadra/update/${quadraId}`)
+            .send({
+                publicplace: 'Rua Atualizada',
+                zipcode: '54321-678',
+                photos: 'link-da-foto-atualizada',
+                type: 'futsal',
+                name: 'Quadra Exemplo Atualizada',
+                state: 'SP',
+                city: 'São Paulo',
+                neighborhood: 'Zona Leste',
+                preco: 'R$150,00',
+                alugado: 'sim',
+                naoalugado: 'não'
+            });
+
+        expect(res.status).toBe(203);
+        expect(res.body.db.name).toBe('Quadra Exemplo Atualizada');
+    });
+
+    it('Deve deletar uma quadra', async () => {
+        const res = await request(app)
+            .delete(`/quadra/del/${quadraId}`)
+
+        expect(res.status).toBe(201);
+    });
 });
 //npm test
